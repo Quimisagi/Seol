469f44952395ba77c3359d14ccbdbcbd92a6b492
cris <cristina.mejia@correounivalle.edu.co>
2019-08-05 09:44:55 -0500

    Organizar urls y pasar todo a español

--- apps/productos/models.py
+++ apps/productos/models.py
@@ -5,19 +5,16 @@ from PIL import Image
 
 class Categoria(models.Model):
     nombre = models.CharField(max_length=50, null=False, unique=True)
-
     estado = models.BooleanField(default = True)
-
 
 class Subcategoria(models.Model):
     nombre = models.CharField(max_length=50, null=False, unique=True)
     estado = models.BooleanField(default = True)
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, null=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, null=True, related_name='subcategorias')
 
     def __str__(self):
         return self.nombre
     
-
 class Producto(models.Model):
     nombre = models.CharField(max_length=50, null=False)
     descripcion = models.TextField()
@@ -26,7 +23,10 @@ class Producto(models.Model):
     marca = models.CharField(max_length=50, null=False)
     estado = models.BooleanField(default = True)
     subcategoria = models.ForeignKey(Subcategoria, on_delete=models.SET_NULL, null=True)
-    imagen = models.ImageField(upload_to='product_pics')
+    imagen = models.ImageField(upload_to='fotos_producto')
+    peso = models.CharField(max_length=50)
+    color = models.CharField(max_length=50)
+    garantia = models.CharField(max_length=50)
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
@@ -37,22 +37,6 @@ class Producto(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.imagen.path)
-
-
-class Detalle(models.Model):
-    nombre = models.CharField(max_length=50, null=False)
-    estado = models.BooleanField(default = True)
-
-
-class Detalle_Producto(models.Model):
-    peso = models.CharField(max_length=50)
-    color = models.CharField(max_length=50)
-    garantia = models.CharField(max_length=50)
-    tamano = models.CharField(max_length=50)
-    densidad = models.CharField(max_length=50)
-    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
-
-
 
 class Descuento_Producto(models.Model):
     fecha_inicio = models.DateField()
