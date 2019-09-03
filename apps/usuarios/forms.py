ead0ca38ecf33da8aa4c5d6a7b4741eb57ecae2d
jesus cuesta <jesus.cuesta@correounivalle.edu.co>
2019-09-03 14:14:45 -0500

    primera implementacion de validacion en fecha nacimiento de usuario

--- apps/usuarios/forms.py
+++ apps/usuarios/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Usuario
 from django.contrib.auth.forms import UserCreationForm
 from .models import Perfil
+from datetime import date
 
 class Formulario_Registrar_Usuario(UserCreationForm):
     nombres = forms.CharField()
@@ -24,6 +25,20 @@ class Formulario_Editar_Usuario(forms.ModelForm):
         model = Usuario
         fields = ['nombres', 'email', 'apellidos', 'telefono', 'direccion', 'fecha_nacimiento']
 
+    #validacion de fecha nacimiento
+    def clean_fecha_nacimiento(self):
+        fechaNacimiento = self.cleaned_data.get('fecha_nacimiento')
+        if calculateAge(fechaNacimiento) < 18:
+            #si es menor de edad
+            raise forms.ValidationError("debes ser mayor de 18 años")
+        return fechaNacimiento
+
+
+def calculateAge(birthDate):
+    days_in_year = 365.2425
+    age = int((date.today() - birthDate).days / days_in_year)
+    return age
+
 class Formulario_Editar_Perfil(forms.ModelForm):
     class Meta:
         model = Perfil
@@ -31,3 +46,5 @@ class Formulario_Editar_Perfil(forms.ModelForm):
         widgets = {
             'imagen': forms.FileInput(attrs={'class': 'bootstrap4-multi-input'})
         }
+
+
